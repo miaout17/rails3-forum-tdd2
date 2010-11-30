af4463aacc51aad032460913eaff71e08bea3397
BigCat <yc_ling@htc.com>
2010-12-01 01:13:05 +0800

    db migration: add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101130170407) do
+ActiveRecord::Schema.define(:version => 20101130171142) do
 
   create_table "forums", :force => true do |t|
     t.string   "title"
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(:version => 20101130170407) do
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                               :default => "", :null => false
-    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
-    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "email",                               :default => "",    :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "",    :null => false
+    t.string   "password_salt",                       :default => "",    :null => false
     t.string   "reset_password_token"
     t.string   "remember_token"
     t.datetime "remember_created_at"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20101130170407) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                               :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
