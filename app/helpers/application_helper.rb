aee3478271fd5e3f958d0683592f2ce4437c6f61
BigCat <yc_ling@htc.com>
2010-11-30 14:06:13 +0800

    ApplicationHelper#auth_menu implement & add into layout/application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def auth_menu
+    if user_signed_in? 
+      links = [
+        link_to("Sign out", destroy_user_session_path)
+      ]
+    else
+      links = [
+        link_to("Sign in", new_user_session_path), 
+        link_to("Sign up", new_user_registration_path)
+      ]
+    end
+
+    content_tag(:div, :class => "auth-menu") do
+      if user_signed_in? 
+        concat "Welcome #{current_user.email}" 
+      else
+        concat "You are not logged in" 
+      end
+      links.each do |link|
+        concat content_tag(:div, link)
+      end
+    end
+  end
 end
