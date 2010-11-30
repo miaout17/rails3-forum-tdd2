865cafa81418a7e2120e8ee6c14e21aff8174c39
BigCat <yc_ling@htc.com>
2010-12-01 01:27:37 +0800

    remove useless Forum actions

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,41 +1,12 @@
 class ForumsController < ApplicationController
-  before_filter :find_forum, :only => [:show, :edit, :update, :destroy]
+  before_filter :find_forum, :only => [:show]
 
   def index
     @forums = Forum.all
   end
 
-  def new
-    @forum = Forum.new
-  end
-
-  def create
-    @forum = Forum.new(params[:forum])
-    if @forum.save
-      redirect_to forum_posts_path(@forum)
-    else
-      render :new
-    end
-  end
-
   def show
     redirect_to forum_posts_path(@forum)
-  end
-
-  def edit
-  end
-
-  def update
-    if @forum.update_attributes(params[:forum])
-      redirect_to forum_posts_path(@forum)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @forum.destroy
-    redirect_to forums_path
   end
 
   protected
--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -31,43 +31,6 @@ describe ForumsController do
     end
   end
 
-  describe "GET new" do
-    it "returns a new forum form" do
-      @forum = mock_model(Forum)
-      Forum.should_receive(:new).and_return(@forum)
-
-      get :new
-
-      assigns(:forum).should eq(@forum)
-      response.should render_template("new")
-    end
-  end
-
-  describe "POST create" do
-    it "creates successfully" do
-      @forum = mock_model(Forum)
-      @params = { "title" => Faker::Lorem.sentence }
-      Forum.should_receive(:new).with(@params).and_return(@forum)
-      @forum.should_receive(:save).and_return(true)
-
-      post :create, {:forum => @params}
-
-      response.should redirect_to(forum_posts_path(@forum))
-    end
-
-    it "fails to create" do
-      @forum = mock_model(Forum)
-      @params = { "title" => Faker::Lorem.sentence }
-      Forum.should_receive(:new).with(@params).and_return(@forum)
-      @forum.should_receive(:save).and_return(false)
-
-      post :create, {:forum => @params}
-
-      assigns(:forum).should eq(@forum)
-      response.should render_template("new")
-    end
-  end
-
   describe "GET show" do
     it "redirect to forum_forums" do
       should_find_forum
@@ -75,51 +38,6 @@ describe ForumsController do
       get :show, :id => 3
 
       response.should redirect_to(forum_posts_path(@forum))
-    end
-  end
-
-  describe "GET edit" do
-    it "returns requested forum" do
-      should_find_forum
-
-      get :edit, :id => 3
-
-      assigns(:forum).should eq(@forum)
-      response.should render_template("edit")
-    end
-  end
-
-  describe "PUT update" do
-    it "update successfully with valid params" do
-      should_find_forum
-      @params = { "title" => Faker::Lorem.sentence }
-      @forum.should_receive(:update_attributes).with(@params).and_return(true)
-
-      put :update, {:id => 3, :forum => @params}
-
-      response.should redirect_to(forum_posts_path(@forum))
-    end
-
-    it "fails to update with invalid params" do
-      should_find_forum
-      @params = { "title" => Faker::Lorem.sentence }
-      @forum.should_receive(:update_attributes).with(@params).and_return(false)
-
-      put :update, {:id => 3, :forum => @params}
-
-      assigns(:forum).should eq(@forum)
-      response.should render_template("edit")
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested forum" do
-      should_find_forum
-      @forum.should_receive(:destroy).and_return(true)
-
-      delete :destroy, {:id => 3}
-
-      response.should redirect_to(forums_path)
     end
   end
 end
