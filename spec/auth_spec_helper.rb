9fca4333ff2fe3e13b997bffdf6f77ff3c79ae61
BigCat <yc_ling@htc.com>
2010-12-01 02:06:06 +0800

    Admin::ForumsController Spec

--- spec/auth_spec_helper.rb
+++ spec/auth_spec_helper.rb
@@ -4,4 +4,15 @@ module AuthSpecHelperMethods
     controller.should_receive(:authenticate_user!)
     controller.stub!(:current_user).and_return(@current_user)
   end
+
+  def should_require_admin
+    @current_user.should_receive(:admin?).and_return(true)
+  end
+
+  def should_redirect_without_admin_premission
+    @current_user.should_receive(:admin?).and_return(false)
+    yield
+    response.should redirect_to "/"
+  end
+
 end
