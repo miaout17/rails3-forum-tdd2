634f0647b1cfafeea46ae4d390f2a5ec047fd094
BigCat <yc_ling@htc.com>
2010-11-30 10:20:09 +0800

    Added 2 specs of forum: * could get its posts * should delete its posts when destroyed

--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -22,4 +22,27 @@ describe Forum do
   it "is invalid without title" do
     Forum.new(@params.except(:title)).should_not be_valid
   end
+
+  it "could get its posts" do
+    forum = Factory(:forum)
+    post = Factory.build(:post, :forum => forum)
+    post.save
+    forum.reload
+    forum.posts.count.should == 1
+    forum.posts.should include(post)
+  end
+
+  describe "is destroyed" do
+    it "also delete its posts" do
+      forum = Factory(:forum)
+      post = Factory(:post, :forum => forum)
+      post_id = post.id
+      Post.find_by_id(post_id).should == post
+
+      forum.destroy
+
+      Post.find_by_id(post_id).should == nil
+    end
+  end
+
 end
