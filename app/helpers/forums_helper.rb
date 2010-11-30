780cc180cfed14680d27a24bec7903a1ca6bf147
BigCat <yc_ling@htc.com>
2010-11-30 23:53:13 +0800

    Coverage is not 100% but it seems rcov estimate error, try to fix it

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,27 +1,32 @@
 module ForumsHelper
   def new_forum_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul,
         content_tag(:li, link_to("Forum list", forums_path))
-      ),
-    :id => "nav")
+      )
+    )
   end
 
   def forum_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul) do
         concat content_tag(:li, link_to("Forum list", forums_path))
         concat content_tag(:li, link_to("Edit forum", edit_forum_path(@forum)))
         concat content_tag(:li, link_to("Destroy forum", forum_path(@forum), :method => :delete))
-      end,
-    :id => "nav")
+      end
+    )
   end
 
   def forums_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul,
         content_tag(:li, link_to("New forum", new_forum_path))
-      ),
-    :id => "nav")
+      )
+    )
+  end
+
+  private
+  def nav_tag(content)
+    content_tag(:div, content, :id => "nav")
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,30 +1,35 @@
 module PostsHelper
   def new_post_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul,
         content_tag(:li, link_to("Post list", forum_posts_path(@forum)))
-      ),
-    :id => "nav")
+      )
+    )
   end
 
   def post_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul) do
         concat content_tag(:li, link_to("Post list", forum_posts_path(@forum)))
         concat content_tag(:li, link_to("Edit post", edit_forum_post_path(@forum, @post)))
         concat content_tag(:li, link_to("Destroy post", forum_post_path(@forum, @post), :method => :delete))
-      end,
-    :id => "nav")
+      end
+    )
   end
 
   def posts_menu
-    content_tag(:div,
+    nav_tag(
       content_tag(:ul) do
         concat content_tag(:li, link_to("Forum list", forums_path))
         concat content_tag(:li, link_to("Edit forum", edit_forum_path(@forum)))
         concat content_tag(:li, link_to("Destroy forum", forum_path(@forum), :method => :delete))
         concat content_tag(:li, link_to("New post", new_forum_post_path(@forum)))
-      end,
-    :id => "nav")
+      end
+    )
+  end
+
+  private
+  def nav_tag(content)
+    content_tag(:div, content, :id => "nav")
   end
 end
