a6f3ee929ea8ed91872c43ead021ec65cf977b37
BigCat <yc_ling@htc.com>
2010-11-30 23:06:58 +0800

    Add spec: post_helper show edit/delete link only when the user is author

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -16,14 +16,35 @@ describe PostsHelper do
     end
 
     describe "#post_menu" do
-      it "display the post menu" do
+
+      before(:each) do
+        @forum = mock_model(Forum)
         @post = mock_model(Post)
+        @current_user = mock_model(User)
+      end
+
+      it "should display edit/delete links if current_user is the author" do
+        helper.stub!(:current_user).and_return(@current_user)
+        @post.stub!(:editable_by?).and_return(true)
+
         assigns[:post] = @post
         menu = helper.post_menu
 
         menu.should =~ /#{forum_posts_path(@forum)}/
         menu.should =~ /#{edit_forum_post_path(@forum, @post)}/
-        menu.should =~ /#{forum_post_path(@forum, @post)}/
+        menu.should =~ /delete/ # the spec is inaccuracy but works
+      end
+
+      it "should not display edit/delete links if current_user isn't the author" do
+        helper.stub!(:current_user).and_return(@current_user)
+        @post.stub!(:editable_by?).and_return(false)
+
+        assigns[:post] = @post
+        menu = helper.post_menu
+
+        menu.should =~ /#{forum_posts_path(@forum)}/
+        menu.should_not =~ /#{edit_forum_post_path(@forum, @post)}/
+        menu.should_not =~ /delete/ # the spec is inaccuracy but works
       end
     end
 
