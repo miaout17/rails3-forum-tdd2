a976b73dddedcdb0a0ca6f9fd7b9295aa228bc5d
BigCat <yc_ling@htc.com>
2010-12-01 02:57:17 +0800

    Implement Admin::ForumsController

--- app/controllers/admin/forums_controller.rb
+++ app/controllers/admin/forums_controller.rb
@@ -1,2 +1,50 @@
 class Admin::ForumsController < ApplicationController
+
+  def index
+    @forums = Forum.all
+  end
+
+  def new
+    @forum = Forum.new
+  end
+
+  def show
+    redirect_to(admin_forum_posts_path(@forum))
+  end
+
+  def edit
+  end
+
+  def create
+    @forum = Forum.new(params[:forum])
+    if @forum.save
+      redirect_to(admin_forum_posts_path(@forum))
+    else
+      render "new"
+    end
+  end
+
+  def update
+    if @forum.update_attributes(params[:forum])
+      redirect_to(admin_forum_posts_path(@forum))
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @forum.destroy
+    redirect_to(admin_forums_path)
+  end
+
+private
+  def find_forum
+    @forum = Forum.find(params[:id])
+  end
+
+  before_filter :authenticate_user!
+  before_filter :admin_required!
+
+  before_filter :find_forum, :only => [:show, :edit, :update, :destroy]
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+private
+  def admin_required!
+    unless current_user.admin?
+      flash.alert="Admin premission required"
+      redirect_to "/"
+    end
+  end
+
 end
