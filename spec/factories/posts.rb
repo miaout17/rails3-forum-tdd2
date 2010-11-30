15b6ae1db91d447dbf32cb41aa43bb2491626e13
BigCat <yc_ling@htc.com>
2010-11-30 10:12:17 +0800

    Using factory_girl on posts and forums

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,4 +1,7 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 Factory.define :post do |f|
+  f.sequence(:title) { Faker::Lorem.sentence }
+  f.sequence(:description) { Faker::Lorem.sentence }
+  f.association :forum
 end
