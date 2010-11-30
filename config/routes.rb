4cbdd12aa36c77a6378cfa95af3d26ef39acaf28
BigCat <yc_ling@htc.com>
2010-12-01 01:20:08 +0800

    modified routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails3PracticeTdd::Application.routes.draw do
   devise_for :users
 
-  root :to => "forums#index"
-
-  resources :forums do
+  resources :forums, :only => [:index, :show] do
     resources :posts
   end
+
+  namespace :admin do
+    resources :forums do
+      resources :posts, :except => [:new, :create]
+    end
+  end
+
+  root :to => "forums#index"
+
 end
